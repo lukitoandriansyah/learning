7779c57ab192b63b238b1c98ebb2e07cafef5657
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-17 08:42:58 +0700

    [JAV-002] SimpleRestAPI

---
+++ Java/springboot-first-app/src/main/java/com/lukito/springbootfirstapp/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.lukito.springbootfirstapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello World";
+    }
+}
