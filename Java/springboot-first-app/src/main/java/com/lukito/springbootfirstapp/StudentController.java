3b4b5b71268f5746683138668d2dfbe14ef10424
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-17 08:59:12 +0700

    [JAV-002] SimpleRestAPI-List JSON Return

--- Java/springboot-first-app/src/main/java/com/lukito/springbootfirstapp/StudentController.java
+++ Java/springboot-first-app/src/main/java/com/lukito/springbootfirstapp/StudentController.java
@@ -3,10 +3,23 @@ package com.lukito.springbootfirstapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("/get-student")
     public Student getStudent(){
         return new Student("Luki","Andri");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Andrian", "Syah1"));
+        students.add(new Student("Andrian", "Syah2"));
+        students.add(new Student("Andrian", "Syah3"));
+        return students;
+
     }
 }
