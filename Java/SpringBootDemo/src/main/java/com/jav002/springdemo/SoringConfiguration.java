3045a39f05eed85ec6e0d13816cfff7f49946650
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-18 17:38:33 +0700

    [JAV-002] Run in cmd

--- Java/SpringBootDemo/src/main/java/com/jav002/springdemo/SoringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.jav002.springdemo;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SoringConfiguration {
-    //Bean Annotation define
-}
