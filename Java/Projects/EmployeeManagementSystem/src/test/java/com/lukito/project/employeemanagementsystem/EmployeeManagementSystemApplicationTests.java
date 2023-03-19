6f97d3c7e494cf8e690165041b6518b1f921caaf
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-19 10:48:28 +0700

    [JAV-002] Project ems

---
+++ Java/Projects/EmployeeManagementSystem/src/test/java/com/lukito/project/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukito.project.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
