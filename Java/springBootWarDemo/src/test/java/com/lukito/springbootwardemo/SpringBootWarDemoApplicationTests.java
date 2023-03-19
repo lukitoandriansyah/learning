ccd71bbecf7b1ca5e80e3413e60396ccb210a80f
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-19 08:17:36 +0700

    [JAV-002] Run war in external tomcat server

---
+++ Java/springBootWarDemo/src/test/java/com/lukito/springbootwardemo/SpringBootWarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukito.springbootwardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
