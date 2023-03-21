606ccca2ddc9575118391752f88db4d2dec6edc4
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-21 10:42:02 +0700

    [PYT-001]-All

--- Python/Day000003/AnyALL/All/Example.py
+++ Python/Day000003/AnyALL/All/Example.py
@@ -1,8 +1,8 @@
-list_a=[False, False, False, False]
+list_a = [False, False, False, False]
 print(all(list_a))
 
-list_b= [False, False, False, True]
+list_b = [False, False, False, True]
 print(all(list_b))
 
-list_c=[True,True,True]
+list_c = [True, True, True]
 print(all(list_c))
