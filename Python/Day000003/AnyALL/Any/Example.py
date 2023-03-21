26268c3aef032df91777f2f021f443902c925608
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-21 10:38:03 +0700

    [PYT-001]-Any

---
+++ Python/Day000003/AnyALL/Any/Example.py
@@ -0,0 +1,8 @@
+#Below it will be returned False
+list_a=[False, False, False, False]
+print(any(list_a))
+
+#Below it will be returend True
+list_b = [False, False, True, False, False]
+print(any(list_b))
+
