df8c63af546417ec6aafff520894f8c161ec22b8
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-21 11:12:00 +0700

    [PYT-001]-basic operator function

---
+++ Python/Day000003/OperatorFunctions/Set1/GtGeNe.py
@@ -0,0 +1,12 @@
+import operator
+
+a, b = 12, 5
+
+# Using gt()
+print("Result using gt(): ", operator.gt(a, b))
+
+# Using ge()
+print("Result using ge(): ", operator.ge(a, b))
+
+# Using ne()
+print("Result using ne(): ", operator.ne(a, b))
