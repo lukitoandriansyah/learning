3e42c87d13d6e8335cca12d937ff9fbb90b73d29
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-21 11:09:41 +0700

    [PYT-001]-basic operator function

---
+++ Python/Day000003/OperatorFunctions/Set1/DivPowMod.py
@@ -0,0 +1,15 @@
+import operator
+
+a, b = 12, 6
+
+# Using truediv()
+print('Result using truediv: ', operator.truediv(a, b))
+
+# Using floordiv()
+print('Result using floordiv: ', operator.floordiv(a, b))
+
+# Using pow()
+print('Result using pow: ', operator.pow(a, b))
+
+# Using mod()
+print('Result using mod: ', operator.mod(a, b))
---
+++ Python/Day000003/OperatorFunctions/Set1/LtLeEq.py
@@ -0,0 +1,12 @@
+import operator
+
+a, b = 12, 5
+
+# Using lt()
+print("Result using lt: ", operator.lt(a, b))
+
+# Using le()
+print("Result using le: ", operator.le(a, b))
+
+# Using eq()
+print("Result using eq: ", operator.eq(a, b))
