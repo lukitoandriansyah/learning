3a9d29316241f13a924c60921855b8d3724e2d4d
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-21 11:02:19 +0700

    [PYT-001]-basic operator function

---
+++ Python/Day000003/OperatorFunctions/Set1/AddSubMul.py
@@ -0,0 +1,12 @@
+import operator
+
+a, b = 12, 7
+
+# Using add() function
+print('This is a result for addition of these two variables: ', operator.add(a, b))
+
+# Using sub() function
+print('This is a result for subtraction of these two variables: ', operator.sub(a, b))
+
+# Using mul() function
+print('This is a result for multiplication of these two variables: ', operator.mul(a, b))
