b8b8387d24d5e0265f4c1178e259d673564f1805
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-25 16:22:28 +0700

    [JAV-002] kafka project

---
+++ Java/Projects/kafka/kafka-producer-wikimedia/src/main/java/com/lukito/kafkaproducersspringboot/SpringBootProducerApplication.java
@@ -0,0 +1,11 @@
+package com.lukito.kafkaproducersspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
