25b6bbd6e46e70d1fbcaf5bbf510ad664b3724e3
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-25 20:08:21 +0700

    [JAV-002] Configured properyieskafka consumer

---
+++ Java/Projects/kafka/kafka-consumer-database/src/main/java/com/lukito/kafkaconsumersdatabases/SpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lukito.kafkaconsumersdatabases;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class, args);
+    }
+
+}
