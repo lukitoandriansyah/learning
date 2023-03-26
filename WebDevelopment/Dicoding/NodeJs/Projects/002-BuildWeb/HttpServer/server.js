2ebe9f1defbd13aa0127303b26cb08e66d700be5
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-26 14:43:01 +0700

    [DIC-001] Initialize HTTP Request Method

--- WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
+++ WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
@@ -1,9 +1,27 @@
 const http = require("http")
 
 const requestListener = (request, response) => {
+    const method = request.method;
     response.setHeader("Content-Type", "text/html")
     response.statusCode = 200;
-    response.end("<h1>This is an HTTP SERVER!</h1>")
+    //Move it into logical
+//    response.end("<h1>This is an HTTP SERVER!</h1>")
+
+    if(method==="GET"){
+        response.end("<h1>This is an HTTP SERVER Response GET!</h1>")
+    }
+
+    if(method==="POST"){
+        response.end("<h1>This is an HTTP SERVER Response POST!</h1>")
+    }
+
+    if(method==="PUT"){
+        response.end("<h1>This is an HTTP SERVER Response PUT!</h1>")
+    }
+
+    if(method==="DELETE"){
+        response.end("<h1>This is an HTTP SERVER Response DELETE!</h1>")
+    }
 }
 
 const server = http.createServer(requestListener)
