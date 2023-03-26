a5cc339754c76a3eff0f67ca0c95704da9e56c92
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-26 15:44:31 +0700

    [DIC-001] Initialize HTTP routing

--- WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
+++ WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
@@ -2,10 +2,46 @@ const http = require("http")
 
 const requestListener = (request, response) => {
     const method = request.method;
+    const url = request.url;
+    const respNotFound = "PAGE NOT FOUND"
+    const respCanNotAccess = "PAGE CAN NOT ACCESSED"
+    //alternatively
+    //const {method, url} = request
     response.setHeader("Content-Type", "text/html")
     response.statusCode = 200;
 
-    if (method === "GET") {
+    if(url==="/"){
+        if (method==="GET"){
+            response.end(`<h1>This is Home Page</h1>`)
+        }else {
+            response.end(`<h1>${respCanNotAccess} with method ${method}</h1>`)
+        }
+
+    }else if (url==="/about"){
+        if (method==="GET"){
+            response.end(`<h1>This is About Page</h1>`)
+        }else if (method==="POST"){
+            //add stream logic to add body
+            let body = [];
+            request.on("data", (chunk)=>{
+                body.push(chunk)
+            })
+
+            request.on("end", ()=>{
+                body = Buffer.concat(body).toString();
+                //const {name} = JSON.parse(body)
+                const arrBody = JSON.parse(body)
+                response.end(`<h1>Hai, ${arrBody.name}! This is an about page</h1>`)
+            })
+        }else {
+            response.end(`<h1>${respCanNotAccess} with method ${method}</h1>`)
+        }
+
+    }else {
+        response.end(`<h1>${respNotFound}</h1>`)
+    }
+
+    /*if (method === "GET") {
         response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }
 
@@ -23,7 +59,7 @@ const requestListener = (request, response) => {
             response.end(`<h1>This is an HTTP SERVER Response ${arrBody.name}!</h1>`)
         })
 
-    }
+    }*/
 
     /*if (method === "PUT") {
         response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
@@ -32,6 +68,8 @@ const requestListener = (request, response) => {
     if (method === "DELETE") {
         response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }*/
+
+
 
 
 }
