91ec35135d1add13c311409754dac770ffbfd487
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-27 03:31:23 +0700

    [DIC-001] Path parameter

--- WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/hapi-web-server/routes.js
+++ WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/hapi-web-server/routes.js
@@ -28,6 +28,14 @@ const routes = [
         }
     },
     {
+        method: "GET",
+        path: "/hello/{user?}",
+        handler: (request, h) => {
+            const {name="stranger"} = request.params
+            return `Hello, ${name}`
+        }
+    },
+    {
         method: "*",
         path: "/{any*}",
         handler: (request, h) => {
