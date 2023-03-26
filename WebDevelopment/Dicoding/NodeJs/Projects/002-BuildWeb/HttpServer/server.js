0e9546fab6350813102fb3b39704dacae1599604
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-26 14:45:46 +0700

    [DIC-001] Initialize HTTP Request Method

--- WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
+++ WebDevelopment/Dicoding/NodeJs/Projects/002-BuildWeb/HttpServer/server.js
@@ -4,30 +4,28 @@ const requestListener = (request, response) => {
     const method = request.method;
     response.setHeader("Content-Type", "text/html")
     response.statusCode = 200;
-    //Move it into logical
-//    response.end("<h1>This is an HTTP SERVER!</h1>")
 
-    if(method==="GET"){
-        response.end("<h1>This is an HTTP SERVER Response GET!</h1>")
+    if (method === "GET") {
+        response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }
 
-    if(method==="POST"){
-        response.end("<h1>This is an HTTP SERVER Response POST!</h1>")
+    if (method === "POST") {
+        response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }
 
-    if(method==="PUT"){
-        response.end("<h1>This is an HTTP SERVER Response PUT!</h1>")
+    if (method === "PUT") {
+        response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }
 
-    if(method==="DELETE"){
-        response.end("<h1>This is an HTTP SERVER Response DELETE!</h1>")
+    if (method === "DELETE") {
+        response.end(`<h1>This is an HTTP SERVER Response ${method}!</h1>`)
     }
 }
 
 const server = http.createServer(requestListener)
-const  port =5000;
-const host="localhost"
+const port = 5000;
+const host = "localhost"
 
-server.listen(port, host, ()=>{
+server.listen(port, host, () => {
     console.log(`Server was running in http://${host}:${port}`)
 })
