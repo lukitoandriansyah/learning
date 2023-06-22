b54407a9da95d31d6123921e60ed2229f2f88eb7
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2023-06-22 21:12:41 +0700

    configure producer method

---
+++ Java/Learning/Springboot-rabbitmq-docker/src/main/java/com/ito/rabbitmqdocker/Springbootrabbitmqdocker/producer/RabbitMQProducer.java
@@ -0,0 +1,28 @@
+package com.ito.rabbitmqdocker.Springbootrabbitmqdocker.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKEY;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent -->> %s",message));
+        rabbitTemplate.convertAndSend(exchange,routingKEY,message);
+    }
+}
