79079b448d3342fe34ed2734f3900db54642f84d
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2023-06-22 20:36:08 +0700

    rabbit initial

---
+++ Java/Learning/Springboot-rabbitmq-docker/src/main/java/com/ito/rabbitmqdocker/Springbootrabbitmqdocker/SpringbootRabbitmqDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ito.rabbitmqdocker.Springbootrabbitmqdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqDockerApplication.class, args);
+	}
+
+}
---
+++ Java/Learning/Springboot-rabbitmq-docker/src/test/java/com/ito/rabbitmqdocker/Springbootrabbitmqdocker/SpringbootRabbitmqDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ito.rabbitmqdocker.Springbootrabbitmqdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
